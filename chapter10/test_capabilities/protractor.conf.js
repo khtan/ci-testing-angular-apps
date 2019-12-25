7f9d1d265832b8b90d891c67e1247adcb452e437
Kwee Tan <kwee@eaglecapsoftware.com>
2019-12-24 17:06:02 -0800

    chapter10/test_capabilities e2e tests added to CircleCI

--- chapter10/test_capabilities/protractor.conf.js
+++ chapter10/test_capabilities/protractor.conf.js
@@ -2,9 +2,7 @@ exports.config = {
   directConnect: true,
   capabilities: {
     browserName: 'chrome',
-    chromeOptions: {
-      args: ['--headless', '--disable-gpu']
-    },
+    chromeOptions: { args: ['--headless', '--disable-gpu']},
     loggingPrefs: {
       performance: 'ALL',
       browser: 'ALL'
@@ -13,6 +11,14 @@ exports.config = {
   baseUrl: 'https://testing-angular-applications.github.io',
   specs: ['e2e/**/*.e2e-spec.ts'],
   onPrepare: () => {
+      let jasmineReporters = require('jasmine-reporters');
+      let junitReporter = new jasmineReporters.JUnitXmlReporter({
+        // setup the output path for the junit reports
+        savePath: 'protractor-results/ch10e2e/',
+        consolidateAll: true
+      });
+      jasmine.getEnv().addReporter(junitReporter);
+    
     require('ts-node').register({
       project: 'e2e'
     });
